d82e3375ad8c04e7925ead44ced1f458c8134a24
Arian Acosta <13475001+arianacosta@users.noreply.github.com>
2019-09-14 19:34:05 -0400

    added release script

---
+++ release/release.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const fse = require("fs-extra");
+const path = require("path");
+const cp = require("child_process");
+
+const currentDir = process.cwd();
+
+const updatePackageBoilerplateJson = () => {
+    const packageJson = path.join(currentDir, "package.json");
+    const packageBoilerplateJson = path.join(currentDir, 'bin/package.boilerplate.json');
+    
+    const package = fse.readJsonSync(packageJson);
+    const packageBoilerplate = fse.readJsonSync(packageBoilerplateJson);
+
+    packageBoilerplate.devDependencies.poetic = `^${package.version}`;
+  
+    fse.writeJsonSync(packageBoilerplateJson, packageBoilerplate, {spaces: 2});
+  }
+
+  updatePackageBoilerplateJson();
