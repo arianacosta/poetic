9e46c7f3d1960ff4b5e9c9b627ff020c04680c69
Oleg Koval <orthodoz@gmail.com>
2019-11-27 10:34:49 +0100

    chore: add husky precommit hooks

---
+++ commitlint.config.js
@@ -0,0 +1,5 @@
+// Configuration for commitlint provided in package.json.
+// This file is needed for configuration to run and override/add rules.
+module.exports = {
+  extends: ["@commitlint/config-conventional"]
+};
