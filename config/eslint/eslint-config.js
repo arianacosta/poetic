2cf4adc2cade956ec11c6558656171d1539aeb7f
Kristoffer Eide <kreid@fiskeridir.no>
2021-01-19 23:26:35 +0100

    fix: update configuration for a mixed JS/TS codebase

--- config/eslint/eslint-config.js
+++ config/eslint/eslint-config.js
@@ -34,6 +34,7 @@ module.exports = {
     "no-unexpected-multiline": "off", // https://github.com/prettier/eslint-config-prettier#no-unexpected-multiline,
     "arrow-parens": ["off"], // handled by prettier with rule 'arrowParens'
     "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }], // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+    "@typescript-eslint/explicit-function-return-type": "off", // https://github.com/typescript-eslint/typescript-eslint/blob/v4.14.0/packages/eslint-plugin/docs/rules/explicit-function-return-type.md#configuring-in-a-mixed-jsts-codebase
     // https://github.com/benmosher/eslint-plugin-import/issues/1568
     "import/extensions": [
       "error",
@@ -54,17 +55,10 @@ module.exports = {
       }
     },
     {
-      files: ["*.js", "*.jsx"],
-      rules: {
-        "@typescript-eslint/no-unused-vars": "off", // duplicate of by no-unused-vars
-        "@typescript-eslint/explicit-function-return-type": "off" // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md#configuring-in-a-mixed-jsts-codebase
-      }
-    },
-    {
-      files: ["*.ts", "*.tsx"],
-      rules: {
-        "no-unused-vars": "off", // duplicate of @typescript-eslint/no-unused-vars
-        "@typescript-eslint/explicit-function-return-type": ["error"] // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md#configuring-in-a-mixed-jsts-codebase
+      // https://github.com/typescript-eslint/typescript-eslint/blob/v4.14.0/packages/eslint-plugin/docs/rules/explicit-function-return-type.md#configuring-in-a-mixed-jsts-codebase
+      "files": ["*.ts", "*.tsx"],
+      "rules": {
+        "@typescript-eslint/explicit-function-return-type": ["error"]
       }
     }
   ]
